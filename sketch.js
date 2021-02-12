d60ca603ce4179ad0ae0bc5f81aab2ff8bbc251b
TitoCodes <titosaid2008@gmail.com>
2021-02-11 19:08:51 -0800

    implemented databse

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 var ball;
 
+var database, BallPos, Pos;
+
 function setup(){
     createCanvas(500,500);
+
+    database = firebase.database();
+    BallPos = database.ref('Ball/pos');
+    BallPos.on("value", readPosition, showError);
+
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
 }
@@ -14,7 +21,7 @@ function draw(){
     else if(keyDown(RIGHT_ARROW)){
         changePosition(1,0);
     }
-    else if(keyDown(UP_ARROW)){
+    if(keyDown(UP_ARROW)){
         changePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
@@ -24,6 +31,19 @@ function draw(){
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    database.ref("Ball/pos").set({'x': Pos.x+x, 'y': Pos.y+y});
+}
+
+function readPosition(data){
+    Pos = data.val();
+    ball.x = Pos.x;
+    ball.y = Pos.y;
+}
+
+function writePosition(x,y){
+    database.ref("Ball/pos").set({'x': Pos.x+x, 'y': Pos.y+y});
+}
+
+function showError(){
+    console.log("bad thing happened");
 }
